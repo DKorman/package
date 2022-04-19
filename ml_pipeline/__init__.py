2e90833a7f36a7a4d8549e7a94f41c4d29c97180
Davor Korman <korman.davor@gmail.com>
2022-04-19 16:05:17 +0200

    initial commit

--- ml_pipeline/__init__.py
+++ ml_pipeline/__init__.py
@@ -22,15 +22,13 @@ __author__ = "davor_korman"
 # from ml_pipeline import utility
 # from ml_pipeline.model_building import XgboostRegressor
 
-from .data_exploratory_analysis import BaseExploratoryDataAnalysis
-# from ml_pipeline import data_exploratory_analysis
-# from ml_pipeline import data_preprocessing
-# from ml_pipeline import data_quality_checking
-# from ml_pipeline import feature_engineering
-# from ml_pipeline import model_building
-# from ml_pipeline import model_eval
-# from ml_pipeline import utility
-# from .model_building import XgboostRegressor
+# from ml_pipeline.data_exploratory_analysis.base_eda import BaseExploratoryDataAnalysis
+# from .data_exploratory_analysis import BaseExploratoryDataAnalysis
+
+import ml_pipeline.data_exploratory_analysis as eda
+
+BaseExploratoryDataAnalysis = eda.base_eda.BaseExploratoryDataAnalysis
+
 
 __all__ = [
     'BaseExploratoryDataAnalysis',
--- ml_pipeline/data_exploratory_analysis/__init__.py
+++ ml_pipeline/data_exploratory_analysis/__init__.py
@@ -4,13 +4,13 @@
 # import ml_pipeline.data_exploratory_analysis.base_eda
 #
 # from base_eda import BaseExploratoryDataAnalysis
-# import .ml_pipeline.data_exploratory_analysis.base_eda.BaseExploratoryDataAnalysis
+# import ml_pipeline.data_exploratory_analysis.base_eda.BaseExploratoryDataAnalysis
 
 # from .base_eda import BaseExploratoryDataAnalysis
 
-from ml_pipeline.data_exploratory_analysis.base_eda import BaseExploratoryDataAnalysis
+# from ml_pipeline.data_exploratory_analysis.base_eda import BaseExploratoryDataAnalysis
 
 
-__all__ = [
-    'BaseExploratoryDataAnalysis'
-]
+# __all__ = [
+#     'BaseExploratoryDataAnalysis'
+# ]
